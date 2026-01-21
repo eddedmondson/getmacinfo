bb24f7a68b2823016e13f6bb05ce3684e4736963
eddedmondson <eddedmondson@gmail.com>
2026-01-21 15:35:57 +0000

    first commit

---
+++ get_id.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "This may take a few minutes, please wait..."
+FILE=$(mktemp)
+system_profiler > $FILE 2> /dev/null
+HUUID=$(grep "Hardware UUID" $FILE)
+SN=$(grep "Serial Number (system)" $FILE)
+MN1=$(grep "^\s*Model Name" $FILE)
+MN2=$(grep "^\s*Model Identifier" $FILE)
+MN3=$(grep "^\s*Model Number" $FILE)
+echo -e "$HUUID\n $SN\n $MN1\n $MN2\n $MN3\n" | pbcopy
+echo "Info copied to clipboard. Please send an email to your IT support or physast.itsupport@ucl.ac.uk pasting the information in (Cmd-V or using the Edit menu)"
+rm $FILE
