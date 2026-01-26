ae520e6f09d11b8d54f7b835425ac4d40541b995
eddedmondson <eddedmondson@gmail.com>
2026-01-26 11:43:38 +0000

    Added computer name to info returned

--- get_id.sh
+++ get_id.sh
@@ -2,11 +2,12 @@
 echo "This may take a few minutes, please wait..."
 FILE=$(mktemp)
 system_profiler > $FILE 2> /dev/null
+NAME=$(scutil --get ComputerName)
 HUUID=$(grep "Hardware UUID" $FILE)
 SN=$(grep "Serial Number (system)" $FILE)
 MN1=$(grep "^\s*Model Name" $FILE)
 MN2=$(grep "^\s*Model Identifier" $FILE)
 MN3=$(grep "^\s*Model Number" $FILE)
-echo -e "$HUUID\n $SN\n $MN1\n $MN2\n $MN3\n" | pbcopy
+echo -e "$NAME\n $HUUID\n $SN\n $MN1\n $MN2\n $MN3\n" | pbcopy
 echo "Info copied to clipboard. Please send an email to your IT support or physast.itsupport@ucl.ac.uk pasting the information in (Cmd-V or using the Edit menu)"
 rm $FILE
