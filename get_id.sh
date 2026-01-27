1b9aafb989c2ed56110477b29b6abbcb9dbc1126
eddedmondson <eddedmondson@gmail.com>
2026-01-27 14:47:14 +0000

    Added MDE device ID to output

--- get_id.sh
+++ get_id.sh
@@ -4,10 +4,11 @@ FILE=$(mktemp)
 system_profiler > $FILE 2> /dev/null
 NAME=$(scutil --get ComputerName)
 HUUID=$(grep "Hardware UUID" $FILE)
+DEVID=$(mdatp health |grep edr_machine_id | sed 's/edr_machine_id[[:space:]]*/Device ID/')
 SN=$(grep "Serial Number (system)" $FILE)
 MN1=$(grep "^\s*Model Name" $FILE)
 MN2=$(grep "^\s*Model Identifier" $FILE)
 MN3=$(grep "^\s*Model Number" $FILE)
-echo -e "$NAME\n $HUUID\n $SN\n $MN1\n $MN2\n $MN3\n" | pbcopy
+echo -e "$NAME\n $HUUID\n $DEVID\n $SN\n $MN1\n $MN2\n $MN3\n" | pbcopy
 echo "Info copied to clipboard. Please send an email to your IT support or physast.itsupport@ucl.ac.uk pasting the information in (Cmd-V or using the Edit menu)"
 rm $FILE
