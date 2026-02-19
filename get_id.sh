098c566455df1a49d1057c5e0b30bf2ccfc509d6
eddedmondson <eddedmondson@gmail.com>
2026-02-19 11:20:15 +0000

    Added device tag, os version, and output to terminal

--- get_id.sh
+++ get_id.sh
@@ -5,10 +5,13 @@ system_profiler > $FILE 2> /dev/null
 NAME=$(scutil --get ComputerName)
 HUUID=$(grep "Hardware UUID" $FILE)
 DEVID=$(mdatp health |grep edr_machine_id | sed 's/edr_machine_id[[:space:]]*/Device ID/')
+TAG=$(mdatp health |grep edr_device_tags)
 SN=$(grep "Serial Number (system)" $FILE)
 MN1=$(grep "^\s*Model Name" $FILE)
 MN2=$(grep "^\s*Model Identifier" $FILE)
 MN3=$(grep "^\s*Model Number" $FILE)
-echo -e "$NAME\n $HUUID\n $DEVID\n $SN\n $MN1\n $MN2\n $MN3\n" | pbcopy
+OSVER=$(sw_vers |grep ProductVersion)
+echo -e "$NAME\n $HUUID\n $DEVID\n $TAG\n $SN\n $MN1\n $MN2\n $MN3\n OS $OSVER\n" | pbcopy
+echo -e "$NAME\n $HUUID\n $DEVID\n $TAG\n $SN\n $MN1\n $MN2\n $MN3\n OS $OSVER\n"
 echo "Info copied to clipboard. Please send an email to your IT support or physast.itsupport@ucl.ac.uk pasting the information in (Cmd-V or using the Edit menu)"
 rm $FILE
